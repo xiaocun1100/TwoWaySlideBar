e415facb6873fd58a2556452746fe163cec1f172
xiaocun1100 <xiaocun1100@163.com>
2020-05-20 16:20:00 +0800

    Initial Commit

---
+++ TwoWaySlideBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwoWaySlideBar
+//
+//  Created by 蒋小寸 on 2020/5/20.
+//  Copyright © 2020 蒋小寸. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
